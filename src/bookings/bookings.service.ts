def89131c9db5dae571556181fc35cc9d54d27a9
Mutai001 <omusekim@gmail.com>
2025-03-27 02:08:00 +0300

    bookin service

--- src/bookings/bookings.service.ts
+++ src/bookings/bookings.service.ts
@@ -1,34 +1,33 @@
-import {eq} from "drizzle-orm";
+import { eq } from "drizzle-orm";
 import db from "../drizzle/db";
- import { TIBookings, TSBookings, bookings } from "../drizzle/schema";
-  export const BookingsService = async (limit?: number): Promise<TSBookings[] | null> => {
-    if (limit) {
-      return await db.query.bookings.findMany({
-        limit: limit
-      });
-    }
-    return await db.query.bookings.findMany();
-  };
+import { TIBookings, bookings } from "../drizzle/schema";
 
-  
+// List Bookings (with optional limit)
+export const BookingsService = async (limit?: number): Promise<TIBookings[] | null> => {
+  return db.query.bookings.findMany({ limit });
+};
+
+// Get a single booking by ID
 export const getBookingsService = async (id: number): Promise<TIBookings | undefined> => {
-    return await db.query.bookings.findFirst({
-        where: eq(bookings.id, id)
-    });
+  return db.query.bookings.findFirst({
+    where: eq(bookings.id, id),
+  });
+};
 
-}
-export const createBookingsService = async (user: TIBookings) => {
-    await db.insert(bookings).values(user)
-    return "booking created successfully";
+// Create a new booking
+export const createBookingsService = async (booking: TIBookings) => {
+  await db.insert(bookings).values(booking);
+  return "Booking created successfully";
+};
 
-}
+// Update a booking by ID
+export const updateBookingsService = async (id: number, booking: TIBookings) => {
+  await db.update(bookings).set(booking).where(eq(bookings.id, id));
+  return "Booking updated successfully";
+};
 
-export const updateBookingsService = async (id: number, user: TIBookings) => {
-    await db.update(bookings).set(user).where(eq(bookings.id, id))
-    return "booking updated successfully";
-}
-
+// Delete a booking by ID
 export const deleteBookingsService = async (id: number) => {
-    await db.delete(bookings).where(eq(bookings.id, id))
-    return "booking deleted successfully";
-}
+  await db.delete(bookings).where(eq(bookings.id, id));
+  return "Booking deleted successfully";
+};
