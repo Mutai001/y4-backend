ae527f7b6a685dd1d7fc9f068ee4116638b7639c
Mutai001 <omusekim@gmail.com>
2025-03-27 01:06:24 +0300

    auth router

--- src/authentication/auth.router.ts
+++ src/authentication/auth.router.ts
@@ -1,21 +1,25 @@
- //authentication/auth.router.ts
- import { Hono } from 'hono';
- import { zValidator } from '@hono/zod-validator';
- import { registerUser, loginUser } from './auth.controller';
- import { registerUserSchema, loginUserSchema } from './validator';
-  
- export const authRouter = new Hono();
-  
- authRouter.post('/register', zValidator('json', registerUserSchema, (result, c) => {
-   if (!result.success) {
-     return c.json(result.error, 400);
-   }
- }), registerUser);
-  
- authRouter.post('/login', zValidator('json', loginUserSchema, (result, c) => {
-   if (!result.success) {
-     return c.json(result.error, 400);
-   }
- }), loginUser);
-  
- 
+// auth.router.ts
+import { Hono } from 'hono';
+import { zValidator } from '@hono/zod-validator';
+import { registerUser, loginUser } from './auth.controller';
+import { registerUserSchema, loginUserSchema } from './validator';
+
+export const authRouter = new Hono();
+
+authRouter.post('/register', 
+    zValidator('json', registerUserSchema, (result, c) => {
+        if (!result.success) {
+            return c.json(result.error.issues, 400);
+        }
+    }), 
+    registerUser
+);
+
+authRouter.post('/login', 
+    zValidator('json', loginUserSchema, (result, c) => {
+        if (!result.success) {
+            return c.json(result.error.issues, 400);
+        }
+    }), 
+    loginUser
+);
