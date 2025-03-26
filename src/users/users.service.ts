20a5ec2e3c1dbc3f721e8e2e2a9d00654260ad80
Mutai001 <omusekim@gmail.com>
2025-03-27 01:59:14 +0300

    user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,34 +1,38 @@
-
 import { eq } from "drizzle-orm";
 import db from "../drizzle/db";
-import { TIUsers, TSUsers, users, } from "../drizzle/schema";
+import { TIUsers, TSUsers, users } from "../drizzle/schema";
 
+// Get a list of users, optionally limiting the results
 export const usersService = async (limit?: number): Promise<TSUsers[] | null> => {
-    if (limit) {
-        return await db.query.users.findMany({
-            limit: limit
-        });
-    }
-    return await db.query.users.findMany();
-}
+  if (limit) {
+    return await db.query.users.findMany({
+      limit: limit,
+    });
+  }
+  return await db.query.users.findMany();
+};
 
+// Get a user by ID
 export const getuserservice = async (id: number): Promise<TIUsers | undefined> => {
-    return await db.query.users.findFirst({
-        where: eq(users.id, id)
-    })
-}
+  return await db.query.users.findFirst({
+    where: eq(users.id, id),
+  });
+};
 
+// Create a new user
 export const createuserservice = async (user: TIUsers) => {
-    await db.insert(users).values(user)
-    return "user created successfully";
-}
+  await db.insert(users).values(user);
+  return "User created successfully";
+};
 
+// Update an existing user by ID
 export const updateuserservice = async (id: number, user: TIUsers) => {
-    await db.update(users).set(user).where(eq(users.id, id))
-    return "user updated successfully";
-}
+  await db.update(users).set(user).where(eq(users.id, id));
+  return "User updated successfully";
+};
 
+// Delete a user by ID
 export const deleteuserservice = async (id: number) => {
-    await db.delete(users).where(eq(users.id, id))
-    return "user deleted successfully";
-}
+  await db.delete(users).where(eq(users.id, id));
+  return "User deleted successfully";
+};
