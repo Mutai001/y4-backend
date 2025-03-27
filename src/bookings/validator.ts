f395fcac162c15b7b90dabb43354e2fbbcd45f8e
Mutai001 <omusekim@gmail.com>
2025-03-27 09:46:42 +0300

    book slot validator

--- src/bookings/validator.ts
+++ src/bookings/validator.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const bookingsSchema = z.object({
-  user_id: z.number(),
-  therapist_id: z.number(),
-  session_date: z.coerce.date(),
-  booking_status: z.enum(['Pending', 'Confirmed', 'Cancelled']),
+  user_id: z.number().int().positive(),
+  therapist_id: z.number().int().positive(),
+  slot_id: z.number().int().positive(),
+  booking_status: z.enum(['Pending', 'Confirmed', 'Cancelled']).default('Pending')
 });
